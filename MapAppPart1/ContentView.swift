8ba6e94599b4ae4854ee63305be915f813334bad
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-10-27 17:40:54 +0900

    Initial Commit

---
+++ MapAppPart1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapAppPart1
+//
+//  Created by 渡邊魁優 on 2023/10/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapAppPart1/MapAppPart1App.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppPart1App.swift
+//  MapAppPart1
+//
+//  Created by 渡邊魁優 on 2023/10/27.
+//
+
+import SwiftUI
+
+@main
+struct MapAppPart1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
